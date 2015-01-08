e6bff4043fbb8b35c4923519462913d748bcffb3
mariomilha <mariomilha@gmail.com>
2015-01-08 19:58:02 +0000

    Inicio da organização

---
+++ ProjectAVE/ProjectTest/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectAVEDLL;
+using ProjectAVE.Entities;
+namespace ProjectTest
+{
+
+    /****************TEST***************/
+    public class Foo
+    {
+        public virtual int DoIt(String v)
+        {
+            Console.WriteLine(
+            "AClass.DoIt() with {0}",
+            v
+            );
+            return v.Length;
+        }
+    }
+
+    class Main
+    {
+        static void Main(string[] args)
+        {
+            IInvocationHandler logInterceptor = new LoggerInterceptor();
+            Foo real = new Foo();
+            Foo proxy = DynamicProxyFactory.MakeProxy<Foo>(
+                            real,
+                            logInterceptor
+             );
+            proxy.DoIt("12");
+        }
+    }
+}
